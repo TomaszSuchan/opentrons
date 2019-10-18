d4763d861f889c3db0ce0967a5ba4a56102047ad
Tomasz <tomasz@MacBook-Pro-Tomasz.local>
2019-10-18 11:26:26 +0200

    Tidy the code

--- dna_purification.ot2.py
+++ dna_purification.ot2.py
@@ -4,43 +4,47 @@ metadata = {
     'protocolName': 'AMPure DNA Purification',
     'author': 'Tomasz Suchan',
     'source': 'Opentrons modified protocol'
-    }
+}
 
 if 'ab-microamp-96-PCR' not in labware.list():
     labware.create(
-        'ab-microamp-96-PCR',  # name of your labware
-        grid=(12, 8),          # number of (columns, rows)
-        spacing=(9.025, 9.025),# distances (mm) between each (column, row)
-        diameter=5.494,        # diameter (mm) of each well
-        depth=22.5,            # depth (mm) of each well
-        volume=200)            # volume (µL) of each well
+        'ab-microamp-96-PCR',    # name of your labware
+        grid=(12, 8),            # number of (columns, rows)
+        spacing=(9.025, 9.025),  # distances (mm) between each (column, row)
+        diameter=5.494,          # diameter (mm) of each well
+        depth=22.5,              # depth (mm) of each well
+        volume=200)              # volume (µL) of each well
 
 if 'brand-8-strip-standard' not in labware.list():
     labware.create(
-        'brand-8-strip-standard',  # name of your labware
-        grid=(12, 8),          # number of (columns, rows)
-        spacing=(9.025, 9.025),# distances (mm) between each (column, row)
-        diameter=5,        # diameter (mm) of each well
-        depth=21,            # depth (mm) of each well
-        volume=200)            # volume (µL) of each well
+        'brand-8-strip-standard',# name of your labware
+        grid=(12, 8),            # number of (columns, rows)
+        spacing=(9.025, 9.025),  # distances (mm) between each (column, row)
+        diameter=5,              # diameter (mm) of each well
+        depth=21,                # depth (mm) of each well
+        volume=200)              # volume (µL) of each well
 
 mag_deck = modules.load('magdeck', '1')
-input_plate = labware.load('opentrons_96_aluminumblock_generic_pcr_strip_200ul', '2')
-output_plate = labware.load('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '3')
+input_plate = labware.load(
+    'opentrons_96_aluminumblock_generic_pcr_strip_200ul', '2')
+output_plate = labware.load(
+    'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '3')
 waste_container = labware.load('agilent_1_reservoir_290ml', '9')
 
+
 def run_custom_protocol(
-        sample_number: int=8,
-        plate_type: 'StringSelection...'='biorad',
-        sample_volume: float=25,
-        bead_ratio: float=1.4,
-        elution_buffer_volume: float=25,
-        incubation_time: float=5,
-        settling_time: float=5,
-        drying_time: float=5):
+        sample_number: int = 8,
+        plate_type: 'StringSelection...' = 'biorad',
+        sample_volume: float = 25,
+        bead_ratio: float = 1.4,
+        elution_buffer_volume: float = 25,
+        incubation_time: float = 5,
+        settling_time: float = 5,
+        drying_time: float = 5):
 
     if plate_type == 'biorad':
-        mag_plate = labware.load('biorad_96_wellplate_200ul_pcr', '1', share=True)
+        mag_plate = labware.load(
+            'biorad_96_wellplate_200ul_pcr', '1', share=True)
         magdeck_height = 18
     elif plate_type == 'microamp':
         mag_plate = labware.load('ab-microamp-96-PCR', '1', share=True)
@@ -49,11 +53,12 @@ def run_custom_protocol(
         mag_plate = labware.load('brand-8-strip-standard', '1', share=True)
         magdeck_height = 22
 
-    #p300
-    total_tips = sample_number*8 + 1
-    tiprack_num = total_tips//96 + (1 if total_tips % 96 > 0 else 0)
+    # p300
+    total_tips = sample_number * 8 + 1
+    tiprack_num = total_tips // 96 + (1 if total_tips % 96 > 0 else 0)
     tips_slots = ['7', '8', '10'][:tiprack_num]
-    tipracks_p300 = [labware.load('opentrons_96_filtertiprack_200ul', slot) for slot in tips_slots]
+    tipracks_p300 = [labware.load(
+        'opentrons_96_filtertiprack_200ul', slot) for slot in tips_slots]
     if sample_number <= 4:
         pipette_p300 = instruments.P300_Single(
             mount='right',
@@ -63,7 +68,7 @@ def run_custom_protocol(
             mount='right',
             tip_racks=tipracks_p300)
 
-    #p50
+    # p50
     tipracks_p50 = [labware.load('opentrons_96_filtertiprack_200ul', '11')]
     pipette_p50 = instruments.P50_Single(
         mount='left',
@@ -74,14 +79,17 @@ def run_custom_protocol(
 
     # Define reagents
     if sample_number <= 4:
-        reagent_container = labware.load('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '4')
-        etoh_container = labware.load('opentrons_6_tuberack_nest_50ml_conical', '5')
+        reagent_container = labware.load(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '4')
+        etoh_container = labware.load(
+            'opentrons_6_tuberack_nest_50ml_conical', '5')
         beads = reagent_container.wells(0)
         elution_buffer = reagent_container.wells(1)
         ethanol = etoh_container.wells(0)
         magplate = [well for well in mag_plate.wells()[:sample_number]]
     else:
-        reagent_container = labware.load('usascientific_12_reservoir_22ml', '4')
+        reagent_container = labware.load(
+            'usascientific_12_reservoir_22ml', '4')
         beads = reagent_container.wells(0)
         elution_buffer = reagent_container.wells(1)
         ethanol = reagent_container.wells(12)
@@ -96,23 +104,24 @@ def run_custom_protocol(
     output = [well for well in output_plate.wells()[:sample_number]]
 
     # Define bead and mix volume
-    bead_volume = sample_volume*bead_ratio
-    if bead_volume/2 > 50:
+    bead_volume = sample_volume * bead_ratio
+    if bead_volume / 2 > 50:
         mix_vol = 50
     else:
-        mix_vol = bead_volume/2
+        mix_vol = bead_volume / 2
     total_vol = bead_volume + sample_volume + 5
 
     # Dispense beads
     pipette_p50.set_flow_rate(dispense=50)
     pipette_p50.distribute(bead_volume,
-                            beads, magplate_samples,
-                            touch_tip=True)
+                           beads, magplate_samples,
+                           touch_tip=True)
 
     # Mix beads and PCR samples
     for target, dest in zip(magplate_samples, samples):
         pipette_p50.pick_up_tip()
-        pipette_p50.transfer(sample_volume, dest, target, new_tip='never', air_gap=air_vol_p50)
+        pipette_p50.transfer(sample_volume, dest, target,
+                             new_tip='never', air_gap=air_vol_p50)
         pipette_p50.mix(10, mix_vol, target)
         pipette_p50.blow_out()
         pipette_p50.drop_tip()
@@ -127,7 +136,8 @@ def run_custom_protocol(
     # Remove supernatant from magnetic beads
     pipette_p300.set_flow_rate(aspirate=25, dispense=150)
     for target in magplate:
-        pipette_p300.transfer(total_vol, target, liquid_waste, air_gap=air_vol_p300, blow_out=True)
+        pipette_p300.transfer(total_vol, target, liquid_waste,
+                              air_gap=air_vol_p300, blow_out=True)
 
     # Wash beads twice with 70% ethanol
     for cycle in range(2):
@@ -148,7 +158,8 @@ def run_custom_protocol(
     # Mix beads with elution buffer
     for target in magplate:
         pipette_p300.pick_up_tip()
-        pipette_p300.transfer(elution_buffer_volume, elution_buffer, target, new_tip='never')
+        pipette_p300.transfer(elution_buffer_volume,
+                              elution_buffer, target, new_tip='never')
         pipette_p300.mix(20, mix_vol, target)
         pipette_p300.drop_tip()
 
@@ -161,7 +172,16 @@ def run_custom_protocol(
 
     # Transfer clean PCR product to a new well
     for target, dest in zip(magplate_samples, output):
-        pipette_p50.transfer(elution_buffer_volume, target, dest, air_gap=air_vol_p50, blow_out=True)
+        pipette_p50.transfer(elution_buffer_volume, target,
+                             dest, air_gap=air_vol_p50, blow_out=True)
+
 
 # Plate type can be: biorad, microamp, strip
-run_custom_protocol(**{'sample_number': 8, 'plate_type': 'strip','sample_volume': 25.0, 'bead_ratio': 1.4, 'elution_buffer_volume': 25.0, 'incubation_time': 0.5, 'settling_time': 0.5, 'drying_time': 0.5})
+run_custom_protocol(**{'sample_number': 8,
+                       'plate_type': 'strip',
+                       'sample_volume': 25.0,
+                       'bead_ratio': 1.4,
+                       'elution_buffer_volume': 25.0,
+                       'incubation_time': 0.5,
+                       'settling_time': 0.5,
+                       'drying_time': 0.5})
